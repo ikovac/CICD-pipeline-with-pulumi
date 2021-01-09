b43c0396ec996b9625ea19dd1e3b2177cef6216e
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-09 18:36:47 +0100

    Remove AfterInstall hook

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,4 +3,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 12
 
-cd /home/ubuntu/app npm i
+cd /home/ubuntu/app
+npm i
+npm i -g pm2
