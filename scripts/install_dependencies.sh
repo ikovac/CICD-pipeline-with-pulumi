7091ec4071564ad83e0072f9223b48cb6c8f023e
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-09 18:47:28 +0100

    Set up nodejs env for all users

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-source ~/.nvm/nvm.sh
-nvm install 12
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash
+echo 'export NVM_DIR="/home/ubuntu/.nvm"' >> /home/ubuntu/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm' >> /home/ubuntu/.bashrc
+# Dot source the files to ensure that variables are available within the current shell
+. /home/ubuntu/.nvm/nvm.sh
+. /home/ubuntu/.profile
+. /home/ubuntu/.bashrc
+# Install NVM, NPM, Node.JS & Grunt
+nvm install --lts
+nvm ls
+EOF
+chown ubuntu:ubuntu /tmp/subscript.sh && chmod a+x /tmp/subscript.sh
+sleep 1; su - ubuntu -c "/tmp/subscript.sh
 
 cd /home/ubuntu/app
 npm i
