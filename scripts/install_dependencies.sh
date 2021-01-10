02b3e64dbb61178879647a385f64e7a17d1c0000
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-10 11:35:22 +0100

    Remove node installation from install script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-source ~/.nvm/nvm.sh
-nvm install 12
-
 sudo chmod 777 -R /home/ubuntu/app
 cd /home/ubuntu/app
 npm i
-npm i -g pm2
