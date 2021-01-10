2b6ea47669ba20c74d76a6afa5a18974925d058a
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-10 13:14:27 +0100

    Add codedeploy group id to the output

--- pulumi/cicd-pipeline/index.js
+++ pulumi/cicd-pipeline/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
 const codePipeline = require('./components/codepipeline');
+const { applicationDeploymentGroup } = require('./components/codedeploy');
 
 exports.codePipeline = codePipeline.name;
+exports.codeDeployGroupId = applicationDeploymentGroup.id;
