0322d590df711d30c362abbe83d94f2b24b0d968
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-10 11:20:37 +0100

    Change app folder permissions

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 12
 
+sudo chmod 777 -R /home/ubuntu/app
 cd /home/ubuntu/app
 npm i
-sudo npm i -g pm2
+npm i -g pm2
