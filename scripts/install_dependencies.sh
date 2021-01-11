e41e3f32832b2e4959fc5fc2af2732c6489bccba
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-11 09:25:00 +0100

    Link nodejs

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 12
+sudo ln -s $(which node) /usr/bin/node
 
 sudo chmod 777 -R /home/ubuntu/app
 cd /home/ubuntu/app
