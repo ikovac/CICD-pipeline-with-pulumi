1326f666b27346e6c9c68125a66f6a5c41da71a8
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-11 10:09:59 +0100

    Link npm

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 12
+sudo ln -s $(which npm) /usr/bin/npm
 sudo ln -s $(which node) /usr/bin/node
 
 npm i -g pm2
