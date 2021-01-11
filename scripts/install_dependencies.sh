a7878cb8d2aaa43c446e03d4be9cbacbd2779482
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-11 09:43:14 +0100

    Echo current folder and pm2 location

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,8 +4,10 @@ source ~/.nvm/nvm.sh
 nvm install 12
 sudo ln -s $(which node) /usr/bin/node
 
-sudo chmod 777 -R /home/ubuntu/app
-cd /home/ubuntu/app
+# sudo chmod 777 -R /home/ubuntu/app
+# cd /home/ubuntu/app
+echo "PWD: $(pwd)"
+echo "PM2: $(which pm2)"
 npm i
 npm i -g pm2
 sudo ln -s $(which pm2) /usr/bin/pm2
